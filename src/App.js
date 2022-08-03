e06bf16eb690f75fa38212c2123199a545801b84
fibre-ether <fire.breather@rediff.com>
2022-08-03 18:56:59 +0530

    Add scroll to top wrapper

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import { useLayoutEffect } from "react";
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 
 function App() {
   return (
-    <div className='font-sans'>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+    <div className="font-sans">
+      <Wrapper>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Wrapper>
     </div>
   );
 }
